3091dfdb2f34ece11e8f875a1a70a33ca0e6cfdc
Aish0611 <77233164+Aish0611@users.noreply.github.com>
2024-05-22 11:47:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Web Developer", "User Interface (UI) Developer", "Website Specialist", "Front-end Developer"],
+        strings: ["Web Designer", "Web Developer", "Ecommerce Marketing Analyst", "User Interface (UI) Developer", "Website Specialist", "Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
